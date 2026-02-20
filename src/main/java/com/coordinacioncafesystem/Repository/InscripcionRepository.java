713cf77d3bc6030b2d13279331564e9f5f2e1001
BryamsSantizo <bryamssantizo1993@gmail.com>
2026-02-20 13:27:08 -0600

    Create InscripcionRepository for Inscripcion entity

---
+++ src/main/java/com/coordinacioncafesystem/Repository/InscripcionRepository.java
@@ -0,0 +1,11 @@
+package com.coordinacioncafesystem.Repository;
+
+import com.coordinacioncafesystem.Entity.Inscripcion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface InscripcionRepository extends JpaRepository<Inscripcion, Long> {
+    List<Inscripcion> findByEstado(String estado);
+}
