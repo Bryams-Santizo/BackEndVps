f1c1eb5852397f4bc3f9a63e41efe9c382d77abf
BryamsSantizo <bryamssantizo1993@gmail.com>
2026-02-20 13:06:29 -0600

    Create Capacitacion.java

---
+++ src/main/java/com/coordinacioncafesystem/Entity/Capacitacion.java
@@ -0,0 +1,169 @@
+package com.coordinacioncafesystem.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "capacitaciones")
+public class Capacitacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String tipo;
+    private String tipoOtro;
+    private String duracion;
+
+    @Column(length = 1000)
+    private String requisitos;
+
+    @Column(columnDefinition = "TEXT")
+    private String competencias;
+
+    private String publicoObjetivo;
+
+    @Column(columnDefinition = "TEXT")
+    private String contenido;
+
+    private String costo;
+    private String emisor;
+    private String disponibilidad;
+    private String materiales;
+    private String criteriosevaluacion;
+    private String instructores;
+
+    private boolean activo = true;
+
+    //Getter and Setter
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTipoOtro() {
+        return tipoOtro;
+    }
+
+    public void setTipoOtro(String tipoOtro) {
+        this.tipoOtro = tipoOtro;
+    }
+
+    public String getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(String duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getRequisitos() {
+        return requisitos;
+    }
+
+    public void setRequisitos(String requisitos) {
+        this.requisitos = requisitos;
+    }
+
+    public String getCompetencias() {
+        return competencias;
+    }
+
+    public void setCompetencias(String competencias) {
+        this.competencias = competencias;
+    }
+
+    public String getPublicoObjetivo() {
+        return publicoObjetivo;
+    }
+
+    public void setPublicoObjetivo(String publicoObjetivo) {
+        this.publicoObjetivo = publicoObjetivo;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public String getCosto() {
+        return costo;
+    }
+
+    public void setCosto(String costo) {
+        this.costo = costo;
+    }
+
+    public String getEmisor() {
+        return emisor;
+    }
+
+    public void setEmisor(String emisor) {
+        this.emisor = emisor;
+    }
+
+    public String getDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(String disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+
+    public String getMateriales() {
+        return materiales;
+    }
+
+    public void setMateriales(String materiales) {
+        this.materiales = materiales;
+    }
+
+    public String getCriteriosevaluacion() {
+        return criteriosevaluacion;
+    }
+
+    public void setCriteriosevaluacion(String criteriosevaluacion) {
+        this.criteriosevaluacion = criteriosevaluacion;
+    }
+
+    public String getInstructores() {
+        return instructores;
+    }
+
+    public void setInstructores(String instructores) {
+        this.instructores = instructores;
+    }
+}
