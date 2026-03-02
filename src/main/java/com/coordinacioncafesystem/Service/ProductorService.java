6bd4d0386cb086e7859713b1fe4ba48bd7342c29
BryamsSantizo <bryamssantizo1993@gmail.com>
2026-03-02 08:51:10 -0600

    Update ProductorService.java

--- src/main/java/com/coordinacioncafesystem/Service/ProductorService.java
+++ src/main/java/com/coordinacioncafesystem/Service/ProductorService.java
@@ -1,4 +1,17 @@
 package com.coordinacioncafesystem.Service;
 
+import com.coordinacioncafesystem.Entity.Certificacion;
+import com.coordinacioncafesystem.Entity.Productores;
+import com.coordinacioncafesystem.Repository.CertificacionRepository;
+import com.coordinacioncafesystem.Repository.ProductorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
 public class ProductorService {
+
+    @Autowired
+    private ProductorRepository repo;
+    public Productores guardar(Productores p) { return repo.save(p); }
 }
