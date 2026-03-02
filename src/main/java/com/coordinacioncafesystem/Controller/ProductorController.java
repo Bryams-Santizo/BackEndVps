999d50355a8fb4bfea7e49a62e6bb9e56aff4193
BryamsSantizo <bryamssantizo1993@gmail.com>
2026-03-01 22:51:38 -0600

    Add ProductorController with crearProductor method

--- src/main/java/com/coordinacioncafesystem/Controller/ProductorController.java
+++ src/main/java/com/coordinacioncafesystem/Controller/ProductorController.java
@@ -1,4 +1,23 @@
 package com.coordinacioncafesystem.Controller;
 
+import com.coordinacioncafesystem.Entity.Productores;
+import com.coordinacioncafesystem.Service.ProductorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/productores")
+@CrossOrigin(origins = "${frontend.origin}")
 public class ProductorController {
+
+    @Autowired
+    private ProductorService productorService;
+
+    // Guarda el formulario del Paso 1
+    @PostMapping
+    public ResponseEntity<Productores> crearProductor(@RequestBody Productores productor) {
+        return ResponseEntity.ok(productorService.guardar(productor));
+    }
+
 }
